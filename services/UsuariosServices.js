b7c2f8bdece98196dcef44692f9831f00e8e4835
Tiago Vieira <tiagosvieira10@gmail.com>
2022-10-20 12:07:03 -0300

    Funcão listar feita

---
+++ services/UsuariosServices.js
@@ -0,0 +1,73 @@
+const usuarios = require('../databases/usuarios.json');
+
+function listar(){
+    console.table(usuarios.map(
+        u => {
+            return{
+                id: u.id,
+                nome: u.nome,
+                email: u.email,
+            }
+        }
+    ))
+}
+
+function salvar(arrayDeUsuarios){
+    // Seu código aqui
+}
+
+function cadastrar(objeto){
+// Seu código aqui
+}
+
+function detalhar(idUsuario){
+// Seu código aqui
+}
+
+function remover(idDoUsuarioParaRemover){
+    // Seu código aqui
+}
+
+function alterar(novosDados, idUsuario){
+    // Seu código aqui
+}
+
+function addEndereco(novoEndereco, idUsuario){
+    // Seu código aqui
+}
+
+function removerEndereco(posicaoDoEndereco, idUsuario){
+// Seu código aqui
+}
+
+function alterarEndereco(posicaoDoEndereco, novoEndereco, idUsuario){
+// Seu código aqui        
+}
+
+function addFormaDePagamento(novaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+function removerFormaDePagamento(posicaoDaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+function alterarFormaDePagamento(novaFormaDePagamento, posicaoDaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+const UsuariosServices = {
+    cadastrar,
+    listar,
+    detalhar,
+    remover,
+    alterar,
+    addEndereco,
+    removerEndereco,
+    alteraEndereco: alterarEndereco,
+    addFormaDePagamento,
+    removerFormaDePagamento,
+    alterarFormaDePagamento
+}
+
+module.exports = UsuariosServices;
---
+++ services/cadastrarUsuario.js
@@ -0,0 +1,21 @@
+/**
+ * Crie uma função cadastrarUsuario
+ * Ela deve receber como parametro um objeto com os seguintes campos:
+ * nome, email, senha, endereco
+ * - Essa função vai criptografar a senha
+ * - Importar um array de usuários
+ * - Criar um objeto com os dados no usuário no formato:
+ * {
+ *  id:__,
+ *  nome: ___,
+ *  email: ___,
+ *  senha: +++,
+ *  enderecos: [___],
+ *  formasDePagamento: []
+ * }
+ * 
+ * O campo id deve ser único para cada usuário
+ * [Bônus] Verificar um usuário com este e-mail já está cadastrado
+ */
+
+function cadastrarUsuario () 
